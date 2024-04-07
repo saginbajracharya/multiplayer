22153f4893de903bf13ef5668e074da8cfe75063
sagin bajracharya <saginbajracharya09@gmail.com>
2024-04-07 11:28:55 +0545

    [Fixed]::Store item listing

--- lib/src/views/store_view/store_view.dart
+++ lib/src/views/store_view/store_view.dart
@@ -7,22 +7,24 @@ import 'package:in_app_purchase_android/billing_client_wrappers.dart';
 import 'package:in_app_purchase_android/in_app_purchase_android.dart';
 import 'package:in_app_purchase_storekit/in_app_purchase_storekit.dart';
 import 'package:in_app_purchase_storekit/store_kit_wrappers.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:multiplayer/src/common/styles.dart';
 import 'consumable_store.dart';
 
 // Auto-consume must be true on iOS.
 // To try without auto-consume on another platform, change `true` to `false` here.
 final bool _kAutoConsume = Platform.isIOS || true;
 
-const String _kConsumableId = 'consumable';
-const String _kUpgradeId = 'upgrade';
-const String _kSilverSubscriptionId = 'subscription_silver';
-const String _kGoldSubscriptionId = 'subscription_gold';
+const String _kConsumableId = '02';
+const String _kUpgradeId = '1';
+// const String _kSilverSubscriptionId = 'subscription_silver';
+// const String _kGoldSubscriptionId = 'subscription_gold';
 
 const List<String> _kProductIds = <String>[
   _kConsumableId,
   _kUpgradeId,
-  _kSilverSubscriptionId,
-  _kGoldSubscriptionId,
+  // _kSilverSubscriptionId,
+  // _kGoldSubscriptionId,
 ];
 
 class StoreView extends StatefulWidget {
@@ -40,6 +42,7 @@ class _StoreViewState extends State<StoreView> {
   List<String> _notFoundIds = <String>[];
   List<ProductDetails> _products = <ProductDetails>[];
   List<PurchaseDetails> _purchases = <PurchaseDetails>[];
+  // ignore: unused_field
   List<String> _consumables = <String>[];
   bool _isAvailable = false;
   bool _purchasePending = false;
@@ -137,10 +140,9 @@ class _StoreViewState extends State<StoreView> {
       stack.add(
         ListView(
           children: <Widget>[
-            _buildConnectionCheckTile(),
+            // _buildConnectionCheckTile(),
             _buildProductList(),
-            _buildConsumableBox(),
-            _buildRestoreButton(),
+            // _buildRestoreButton(),
           ],
         ),
       );
@@ -164,57 +166,58 @@ class _StoreViewState extends State<StoreView> {
         ),
       );
     }
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('IAP Example'),
+        automaticallyImplyLeading: true,
+        backgroundColor: transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: white),
+        centerTitle: true,
+        title: Text(AppLocalizations.of(context)!.store,style: const TextStyle(color:white)),
       ),
+      extendBodyBehindAppBar: true,
       body: Stack(
         children: stack,
       ),
     );
   }
 
-  Card _buildConnectionCheckTile() {
-    if (_loading) {
-      return const Card(child: ListTile(title: Text('Trying to connect...')));
-    }
-    final Widget storeHeader = ListTile(
-      leading: Icon(
-        _isAvailable ? Icons.check : Icons.block,
-        color: _isAvailable
-        ? Colors.green
-        : ThemeData.light().colorScheme.error
-      ),
-      title: Text('The store is ${_isAvailable ? 'available' : 'unavailable'}.'),
-    );
-    final List<Widget> children = <Widget>[storeHeader];
+  // Card _buildConnectionCheckTile() {
+  //   if (_loading) {
+  //     return const Card(child: ListTile(title: Text('Trying to connect...')));
+  //   }
+  //   final Widget storeHeader = ListTile(
+  //     leading: Icon(
+  //       _isAvailable ? Icons.check : Icons.block,
+  //       color: _isAvailable
+  //       ? Colors.green
+  //       : ThemeData.light().colorScheme.error
+  //     ),
+  //     title: Text('The store is ${_isAvailable ? 'available' : 'unavailable'}.'),
+  //   );
+  //   final List<Widget> children = <Widget>[storeHeader];
 
-    if (!_isAvailable) {
-      children.addAll(<Widget>[
-        const Divider(),
-        ListTile(
-          title: Text('Not connected',style: TextStyle(color: ThemeData.light().colorScheme.error)),
-          subtitle: const Text('Unable to connect to the payments processor. Has this app been configured correctly? See the example README for instructions.'),
-        ),
-      ]);
-    }
-    return Card(child: Column(children: children));
-  }
+  //   if (!_isAvailable) {
+  //     children.addAll(<Widget>[
+  //       const Divider(),
+  //       ListTile(
+  //         title: Text('Not connected',style: TextStyle(color: ThemeData.light().colorScheme.error)),
+  //         subtitle: const Text('Unable to connect to the payments processor. Has this app been configured correctly? See the example README for instructions.'),
+  //       ),
+  //     ]);
+  //   }
+  //   return Card(child: Column(children: children));
+  // }
 
-  Card _buildProductList() {
+  Widget _buildProductList() {
     if (_loading) {
-      return const Card(
-        child: ListTile(
-          leading: CircularProgressIndicator(),
-          title: Text('Fetching products...')
-        )
+      return const Center(
+        child: CircularProgressIndicator()
       );
     }
     if (!_isAvailable) {
-      return const Card();
+      return const SizedBox();
     }
-    const ListTile productHeader = ListTile(title: Text('Products for Sale'));
     final List<ListTile> productList = <ListTile>[];
     if (_notFoundIds.isNotEmpty) {
       productList.add(ListTile(
@@ -222,13 +225,13 @@ class _StoreViewState extends State<StoreView> {
             '[${_notFoundIds.join(", ")}] not found',
             style: TextStyle(color: ThemeData.light().colorScheme.error)
           ),
-          subtitle: const Text(
-            'This app needs special configuration to run. Please see example/README.md for instructions.'
+          subtitle: Text(
+            'This app needs special configuration to run. Please see example/README.md for instructions.',
+            style: normalTextStyle
           )
         )
       );
     }
-
     // This loading previous purchases code is just a demo. Please do not use this as it is.
     // In your app you should always verify the purchase data using the `verificationData` inside the [PurchaseDetails] object before trusting it.
     // We recommend that you use your own server to verify the purchase data.
@@ -243,9 +246,11 @@ class _StoreViewState extends State<StoreView> {
         return ListTile(
           title: Text(
             productDetails.title,
+            style: normalTextStyle
           ),
           subtitle: Text(
             productDetails.description,
+            style: normalTextStyle
           ),
           trailing: previousPurchase != null && Platform.isIOS
           ? IconButton(
@@ -293,76 +298,74 @@ class _StoreViewState extends State<StoreView> {
         );
       },
     ));
-    return Card(
-      child: Column(
-        children: <Widget>[productHeader, const Divider()] + productList
-      )
+    return Column(
+      children: productList
     );
   }
 
-  Card _buildConsumableBox() {
-    if (_loading) {
-      return const Card(
-        child: ListTile(
-          leading: CircularProgressIndicator(),
-          title: Text('Fetching consumables...')
-        )
-      );
-    }
-    if (!_isAvailable || _notFoundIds.contains(_kConsumableId)) {
-      return const Card();
-    }
-    const ListTile consumableHeader = ListTile(title: Text('Purchased consumables'));
-    final List<Widget> tokens = _consumables.map((String id) {
-      return GridTile(
-        child: IconButton(
-          icon: const Icon(
-            Icons.stars,
-            size: 42.0,
-            color: Colors.orange,
-          ),
-          splashColor: Colors.yellowAccent,
-          onPressed: () => consume(id),
-        ),
-      );
-    }).toList();
-    return Card(
-      child: Column(
-        children: <Widget>[
-          consumableHeader,
-          const Divider(),
-          GridView.count(
-            crossAxisCount: 5,
-            shrinkWrap: true,
-            padding: const EdgeInsets.all(16.0),
-            children: tokens,
-          )
-        ]
-      )
-    );
-  }
+  // Card _buildConsumableBox() {
+  //   if (_loading) {
+  //     return const Card(
+  //       child: ListTile(
+  //         leading: CircularProgressIndicator(),
+  //         title: Text('Fetching consumables...')
+  //       )
+  //     );
+  //   }
+  //   if (!_isAvailable || _notFoundIds.contains(_kConsumableId)) {
+  //     return const Card();
+  //   }
+  //   const ListTile consumableHeader = ListTile(title: Text('Purchased consumables'));
+  //   final List<Widget> tokens = _consumables.map((String id) {
+  //     return GridTile(
+  //       child: IconButton(
+  //         icon: const Icon(
+  //           Icons.stars,
+  //           size: 42.0,
+  //           color: Colors.orange,
+  //         ),
+  //         splashColor: Colors.yellowAccent,
+  //         onPressed: () => consume(id),
+  //       ),
+  //     );
+  //   }).toList();
+  //   return Card(
+  //     child: Column(
+  //       children: <Widget>[
+  //         consumableHeader,
+  //         const Divider(),
+  //         GridView.count(
+  //           crossAxisCount: 5,
+  //           shrinkWrap: true,
+  //           padding: const EdgeInsets.all(16.0),
+  //           children: tokens,
+  //         )
+  //       ]
+  //     )
+  //   );
+  // }
 
-  Widget _buildRestoreButton() {
-    if (_loading) {
-      return Container();
-    }
-    return Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          TextButton(
-            style: TextButton.styleFrom(
-              backgroundColor: Theme.of(context).primaryColor,
-              foregroundColor: Colors.white,
-            ),
-            onPressed: () => _inAppPurchase.restorePurchases(),
-            child: const Text('Restore purchases'),
-          ),
-        ],
-      ),
-    );
-  }
+  // Widget _buildRestoreButton() {
+  //   if (_loading) {
+  //     return Container();
+  //   }
+  //   return Padding(
+  //     padding: const EdgeInsets.all(4.0),
+  //     child: Row(
+  //       mainAxisAlignment: MainAxisAlignment.end,
+  //       children: <Widget>[
+  //         TextButton(
+  //           style: TextButton.styleFrom(
+  //             backgroundColor: Theme.of(context).primaryColor,
+  //             foregroundColor: Colors.white,
+  //           ),
+  //           onPressed: () => _inAppPurchase.restorePurchases(),
+  //           child: const Text('Restore purchases'),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 
   Future<void> consume(String id) async {
     await ConsumableStore.consume(id);
@@ -453,6 +456,8 @@ class _StoreViewState extends State<StoreView> {
   }
 
   GooglePlayPurchaseDetails? _getOldSubscription(ProductDetails productDetails, Map<String, PurchaseDetails> purchases) {
+    return null;
+  
     // This is just to demonstrate a subscription upgrade or downgrade.
     // This method assumes that you have only 2 subscriptions under a group, 'subscription_silver' & 'subscription_gold'.
     // The 'subscription_silver' subscription can be upgraded to 'subscription_gold' and
@@ -460,17 +465,17 @@ class _StoreViewState extends State<StoreView> {
     // Please remember to replace the logic of finding the old subscription Id as per your app.
     // The old subscription is only required on Android since Apple handles this internally
     // by using the subscription group feature in iTunesConnect.
-    GooglePlayPurchaseDetails? oldSubscription;
-    if (productDetails.id == _kSilverSubscriptionId &&
-        purchases[_kGoldSubscriptionId] != null) {
-      oldSubscription =
-          purchases[_kGoldSubscriptionId]! as GooglePlayPurchaseDetails;
-    } else if (productDetails.id == _kGoldSubscriptionId &&
-        purchases[_kSilverSubscriptionId] != null) {
-      oldSubscription =
-          purchases[_kSilverSubscriptionId]! as GooglePlayPurchaseDetails;
-    }
-    return oldSubscription;
+    // GooglePlayPurchaseDetails? oldSubscription;
+    // if (productDetails.id == _kSilverSubscriptionId &&
+    //     purchases[_kGoldSubscriptionId] != null) {
+    //   oldSubscription =
+    //       purchases[_kGoldSubscriptionId]! as GooglePlayPurchaseDetails;
+    // } else if (productDetails.id == _kGoldSubscriptionId &&
+    //     purchases[_kSilverSubscriptionId] != null) {
+    //   oldSubscription =
+    //       purchases[_kSilverSubscriptionId]! as GooglePlayPurchaseDetails;
+    // }
+    // return oldSubscription;
   }
 }
 
